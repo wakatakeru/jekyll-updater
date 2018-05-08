f183011762119054a58f332e12081257ac232a4c
Takeru SHIMIZU <s15t233@stu.kagawa-u.ac.jp>
2018-05-08 09:52:30 +0000

    updateの動作を実装

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ruby:2.5.0
-
-ADD . /app
-WORKDIR /app
-RUN bundle install -j4
--- src/receiver.rb
+++ src/receiver.rb
@@ -1,14 +1,36 @@
+require 'dotenv'
 require 'sinatra'
+require 'open3'
+
+Dotenv.load
 
 post '/' do
   request.body.rewind
   payload_body = request.body.read
   verify_signature(payload_body)
   push = JSON.parse(params[:payload])
-  p push
+  update_repo if master_merged?(push)
 end
 
 def verify_signature(payload_body)
   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
   return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+end
+
+def master_merged?(push)
+  action = push['action']
+  ref = push['pull_request']['base']['ref']
+  merged = push['pull_request']['merged']
+
+  return true if action == 'closed' && ref == 'master' && merged == true
+  return false
+end
+
+def update_repo
+  jekyll_root = ENV['JEKYLL_ROOT'].chomp
+
+  Dir.chdir(jekyll_root) do
+    `git checkout master`
+    `git pull origin master`
+  end
 end
